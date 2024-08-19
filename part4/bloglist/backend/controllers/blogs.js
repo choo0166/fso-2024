51a620c7b38fb9246da7cbf98dd14957aaf8f8a7
chooyl <chooyl0235@gmail.com>
2024-08-19 23:09:15 +0800

    refactor route event handlers to async/await

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -2,24 +2,54 @@ const blogsRouter = require("express").Router()
 const logger = require("../utils/logger")
 const Blog = require("../models/blog")
 
-blogsRouter.get("/", (request, response, next) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs)
-    })
-    .catch((error) => next(error))
+blogsRouter.get("/", async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({})
+    response.json(blogs)
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogsRouter.post("/", (request, response, next) => {
+blogsRouter.post("/", async (request, response, next) => {
   const newBlog = new Blog(request.body)
+  try {
+    const savedBlog = await newBlog.save()
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    next(error)
+  }
+})
 
-  newBlog
-    .save()
-    .then((savedBlog) => {
-      // return formatted object from transformed toJSON method
-      response.status(201).json(savedBlog)
+blogsRouter.delete("/:id", async (request, response, next) => {
+  const id = request.params.id
+  try {
+    const res = await Blog.findByIdAndDelete(id)
+    logger.info(res)
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.put("/:id", async (request, response, next) => {
+  const id = request.params.id
+  const payload = request.body
+
+  try {
+    const res = await Blog.findByIdAndUpdate(id, payload, {
+      new: true,
+      runValidators: true,
+      context: "query",
     })
-    .catch((error) => next(error))
+    if (res) {
+      response.json(res)
+    } else {
+      response.status(404).json({ error: "Record does not exist on server" })
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = blogsRouter
